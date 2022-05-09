1b53ab897224e9bda7bbc7cf0bd55a7607093130
Evgengrmit <evgengrmit@icloud.com>
2022-05-09 15:27:58 +0300

    add new migration exercise

--- migration/000001_init.down.sql
+++ migration/000001_init.down.sql
@@ -1,4 +1,3 @@
 DROP TABLE workout_day;
 DROP TABLE schedule;
 DROP TABLE trainer;
-DROP TABLE exercise;
--- migration/000001_init.up.sql
+++ migration/000001_init.up.sql
@@ -19,9 +19,3 @@ CREATE TABLE schedule
     scheduled_at timestamp    not null,
     trainer_id   int references trainer (id)
 );
-CREATE TABLE exercise
-(
-    id         bigserial not null primary key,
-    name       varchar   not null,
-    created_at timestamp not null default current_date
-)
