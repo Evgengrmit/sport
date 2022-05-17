cb75106845bded73396548a96264879226ff711f
Evgengrmit <evgengrmit@icloud.com>
2022-05-17 18:11:39 +0300

    fix current date to current timestamp in workoutResult

--- migration/20220517095021_workoutResult_update_table.down.sql
+++ migration/20220517095021_workoutResult_update_table.down.sql
@@ -1,1 +1,2 @@
 ALTER TABLE workout_result DROP COLUMN user_name;
+ALTER TABLE workout_result ALTER COLUMN created_at SET DEFAULT current_date;
--- migration/20220517095021_workoutResult_update_table.up.sql
+++ migration/20220517095021_workoutResult_update_table.up.sql
@@ -1,1 +1,3 @@
 ALTER TABLE workout_result ADD COLUMN user_name varchar(255) null;
+ALTER TABLE workout_result ALTER created_at TYPE timestamptz;
+ALTER TABLE workout_result ALTER COLUMN created_at SET DEFAULT current_timestamp;
