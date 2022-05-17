2da0767a0164c0d3a010da73eb925fac41bf47ce
Evgengrmit <evgengrmit@icloud.com>
2022-05-17 18:23:11 +0300

    add down.sql

--- migration/20220517095021_workoutResult_update_table.down.sql
+++ migration/20220517095021_workoutResult_update_table.down.sql
@@ -1,2 +1,3 @@
 ALTER TABLE workout_result DROP COLUMN user_name;
+ALTER TABLE workout_result ALTER created_at TYPE timestamp;
 ALTER TABLE workout_result ALTER COLUMN created_at SET DEFAULT current_date;
