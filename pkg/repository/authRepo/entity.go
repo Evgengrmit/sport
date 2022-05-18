d8bf3fada7518335640449cde7272a6150389356
Evgengrmit <evgengrmit@icloud.com>
2022-05-18 21:52:07 +0300

    add phone number in login user

--- pkg/repository/authRepo/entity.go
+++ pkg/repository/authRepo/entity.go
@@ -5,12 +5,14 @@ import "github.com/jmoiron/sqlx"
 type User struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
-	Email string `json:"email"`
+	Email string `json:"email,omitempty"`
+	Phone string `json:"phone,omitempty"`
 }
 type AuthorizationRepo interface {
 	CreateUser(u User) (User, error)
-	IsUserExists(u User) (bool, error)
+	IsUserExistsByEmail(u User) (bool, error)
 	IsUserExistsByID(uID int) (bool, error)
+	IsUserExistsByPhone(u User) (bool, error)
 }
 
 type Authorization struct {
--- pkg/repository/authRepo/repository.go
+++ pkg/repository/authRepo/repository.go
@@ -3,6 +3,7 @@ package authRepo
 import (
 	"errors"
 	"github.com/jmoiron/sqlx"
+	"regexp"
 )
 
 func NewAuthorization(db *sqlx.DB) *Authorization {
@@ -10,22 +11,70 @@ func NewAuthorization(db *sqlx.DB) *Authorization {
 }
 
 func (a *Authorization) CreateUser(u User) (User, error) {
-	status, err := a.IsUserExists(u)
-	if err != nil {
-		return User{}, err
+	if u.Email != "" && u.Phone == "" {
+
+		statusEmail, err := a.IsUserExistsByEmail(u)
+		if err != nil {
+			return User{}, err
+		}
+		if statusEmail {
+			return User{}, errors.New("user with this email exists")
+		}
+		err = a.db.DB.QueryRow("INSERT INTO users (name, email) VALUES ($1,$2) RETURNING id",
+			u.Name, u.Email).Scan(&u.ID)
+		return u, err
+	} else if u.Email == "" && u.Phone != "" {
+		matched, err := regexp.MatchString(`^(7\d{10})?$`, u.Phone)
+		if !matched || err != nil {
+			return User{}, errors.New("incorrect phone number format")
+		}
+		statusPhone, err := a.IsUserExistsByPhone(u)
+		if err != nil {
+			return User{}, err
+		}
+		if statusPhone {
+			return User{}, errors.New("user with this phone exists")
+		}
+		err = a.db.DB.QueryRow("INSERT INTO users (name, phone) VALUES ($1,$2) RETURNING id",
+			u.Name, u.Phone).Scan(&u.ID)
+		return u, err
+	} else if u.Email != "" && u.Phone != "" {
+		matched, err := regexp.MatchString(`^(7\d{10})?$`, u.Phone)
+		if !matched || err != nil {
+			return User{}, errors.New("incorrect phone number format")
+		}
+		statusPhone, err := a.IsUserExistsByPhone(u)
+		if err != nil {
+			return User{}, err
+		}
+		if statusPhone {
+			return User{}, errors.New("user with this phone exists")
+		}
+		statusEmail, err := a.IsUserExistsByEmail(u)
+		if err != nil {
+			return User{}, err
+		}
+		if statusEmail {
+			return User{}, errors.New("user with this email exists")
+		}
+		err = a.db.DB.QueryRow("INSERT INTO users (name, email,phone) VALUES ($1,$2,$3) RETURNING id",
+			u.Name, u.Email, u.Phone).Scan(&u.ID)
+		return u, err
 	}
-	if status {
-		return User{}, errors.New("user with this email exists")
-	}
-	err = a.db.DB.QueryRow("INSERT INTO users (name, email) VALUES ($1,$2) RETURNING id",
-		u.Name, u.Email).Scan(&u.ID)
-	return u, err
+	return User{}, errors.New("there must be a phone number or email")
 }
 
-func (a *Authorization) IsUserExists(u User) (bool, error) {
+func (a *Authorization) IsUserExistsByEmail(u User) (bool, error) {
 	var exists bool
 	err := a.db.DB.QueryRow("SELECT EXISTS(SELECT * FROM users WHERE email = $1)",
 		u.Email).Scan(&exists)
+	return exists, err
+}
+
+func (a *Authorization) IsUserExistsByPhone(u User) (bool, error) {
+	var exists bool
+	err := a.db.DB.QueryRow("SELECT EXISTS(SELECT * FROM users WHERE phone = $1)",
+		u.Phone).Scan(&exists)
 	return exists, err
 }
 
